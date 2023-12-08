568aeb389ba3b038088bb4b633428347a867aa77
MercyKorir <mcthegreat78@gmail.com>
2023-12-09 00:10:35 +0300

    Update Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,7 @@ $routes->post('/course/update/(:segment)', 'Course::update/$1');
 $routes->resource('user', ['controller' => 'User']);
 $routes->options('user', 'User::options');
 $routes->options('user/login', 'User::options');
+$routes->options('user/register', 'User::options');
 $routes->post('/user/login', 'User::login');
 $routes->post('/user/register', 'User::register');
 // $routes->post('/user/logout', 'User::logout');
