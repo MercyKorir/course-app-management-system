8402717327f046c06c0bbf629d73505741503dea
MercyKorir <mcthegreat78@gmail.com>
2024-02-24 06:59:32 +0300

    Implemented styling on active user nav menu

--- src/components/UserNavigation.jsx
+++ src/components/UserNavigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
@@ -6,21 +6,42 @@ import LoginIcon from "@mui/icons-material/Login";
 import styles from "../styles/UserNavigation.module.css";
 
 const UserNavigation = () => {
+  const [activeMenuItem, setActiveMenuItem] = useState("home");
   return (
     <div className={styles.userNavContainer}>
-      <div className={styles.userMenuItem}>
+      <div
+        className={`${styles.userMenuItem} ${
+          activeMenuItem === "home" && styles.active
+        }`}
+        onClick={() => setActiveMenuItem("home")}
+      >
         <HomeIcon />
         <p>Home</p>
       </div>
-      <div className={styles.userMenuItem}>
+      <div
+        className={`${styles.userMenuItem} ${
+          activeMenuItem === "notifications" && styles.active
+        }`}
+        onClick={() => setActiveMenuItem("notifications")}
+      >
         <NotificationsIcon />
         <p>Notifications</p>
       </div>
-      <div className={styles.userMenuItem}>
+      <div
+        className={`${styles.userMenuItem} ${
+          activeMenuItem === "support" && styles.active
+        }`}
+        onClick={() => setActiveMenuItem("support")}
+      >
         <SupportAgentIcon />
         <p>Support</p>
       </div>
-      <div className={styles.userMenuItem}>
+      <div
+        className={`${styles.userMenuItem} ${
+          activeMenuItem === "login" && styles.active
+        }`}
+        onClick={() => setActiveMenuItem("login")}
+      >
         <LoginIcon />
         <p>Login</p>
       </div>
