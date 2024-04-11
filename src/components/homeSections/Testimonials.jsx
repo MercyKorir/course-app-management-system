def5e6a9d613636295ac70af31f9587ed38eada8
MercyKorir <mcthegreat78@gmail.com>
2024-04-11 17:14:39 +0300

    Updated Testimonials to implement functionalities and added animation

--- src/components/homeSections/Testimonials.jsx
+++ src/components/homeSections/Testimonials.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/Testimonials.module.css";
 
 const Testimonials = () => {
+  const testimonialsData = [
+    {
+      name: "Random User 1",
+      email: "testuser@example.com",
+      testimony:
+        '"Taking courses on this platform has been a game-changer for me. The content is top-notch, the instructors are experts in their fields, and the learning experience is truly enjoyable. I\'ve gained valuable skills that have helped me advance in my career. Highly recommended!"',
+    },
+    {
+      name: "Random User 2",
+      email: "testuser2@example.com",
+      testimony:
+        '"Taking courses on this platform has been a game-changer for me. The content is top-notch, the instructors are experts in their fields, and the learning experience is truly enjoyable. Highly recommended!"',
+    },
+    {
+      name: "Random User 3",
+      email: "testuser3@example.com",
+      testimony:
+        '"Taking courses on this platform has been a game-changer for me. The content is top-notch, the instructors are experts in their fields, and the learning experience is truly enjoyable."',
+    },
+  ];
+
+  const [currentTestimonialIndex, setCurrentTestimonialIndex] = useState(0);
+  const [showTestimonial, setShowTestimonial] = useState(false);
+
+  useEffect(() => {
+    setShowTestimonial(false);
+
+    const timer = setTimeout(() => {
+      setShowTestimonial(true);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [currentTestimonialIndex]);
+
+  const handleNextTestimonial = () => {
+    setCurrentTestimonialIndex((prevIndex) =>
+      prevIndex === testimonialsData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevTestimonial = () => {
+    setCurrentTestimonialIndex((prevIndex) =>
+      prevIndex === 0 ? testimonialsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div id="testimonials" className={styles.testimonialsContainer}>
       <div className={styles.testimonialsContent}>
@@ -34,25 +80,37 @@ const Testimonials = () => {
             <div className={styles.testimonialCard}>
               <div className={styles.testimonialCardContent}>
                 <div className={styles.prevNextBtns}>
-                  <button className={styles.prevBtn}>&#60;</button>
-                  <button className={styles.nextBtn}>&#62;</button>
+                  <button
+                    className={styles.prevBtn}
+                    onClick={handlePrevTestimonial}
+                  >
+                    &#60;
+                  </button>
+                  <button
+                    className={styles.nextBtn}
+                    onClick={handleNextTestimonial}
+                  >
+                    &#62;
+                  </button>
                 </div>
                 <div className={styles.userTestimonial}>
-                  <div className={styles.userInfo}>
+                  <div
+                    className={`${styles.userInfo} ${
+                      showTestimonial ? styles.show : ""
+                    }`}
+                  >
                     <div className={styles.userImg}></div>
                     <div className={styles.userNameDetails}>
-                      <h3>Random User</h3>
-                      <p>testuser@example.com</p>
+                      <h3>{testimonialsData[currentTestimonialIndex].name}</h3>
+                      <p>{testimonialsData[currentTestimonialIndex].email}</p>
                     </div>
                   </div>
-                  <div className={styles.userTestimony}>
-                    <p>
-                      "Taking courses on this platform has been a game-changer
-                      for me. The content is top-notch, the instructors are
-                      experts in their fields, and the learning experience is
-                      truly enjoyable. I've gained valuable skills that have
-                      helped me advance in my career. Highly recommended!"
-                    </p>
+                  <div
+                    className={`${styles.userTestimony} ${
+                      showTestimonial ? styles.show : ""
+                    }`}
+                  >
+                    <p>{testimonialsData[currentTestimonialIndex].testimony}</p>
                   </div>
                 </div>
               </div>
