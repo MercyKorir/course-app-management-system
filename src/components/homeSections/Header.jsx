429ed2e0af874d785b4ba6968a13a313c44dae43
MercyKorir <mcthegreat78@gmail.com>
2024-04-11 16:22:35 +0300

    Updated Header to implement functionalities

--- src/components/homeSections/Header.jsx
+++ src/components/homeSections/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../../styles/Header.module.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div id="header" className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <h1 className={styles.logoHeaderText}>LOGO</h1>
-        <button type="button" className={styles.loginBtn}>
+        <Link to={"/"} style={{textDecoration: "none"}}>
+          <h1 className={styles.logoHeaderText}>LOGO</h1>
+        </Link>
+        <button type="button" className={styles.loginBtn} onClick={handleLogin}>
           Login
         </button>
       </div>
